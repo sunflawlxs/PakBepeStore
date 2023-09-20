9e110ab38024e6328bb764d2c392ad3855aab569
sunflawlxs <sherylivana99@gmail.com>
2023-09-20 15:02:56 +0700

    tugas 3

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/user/Documents/TUGAS2PBP/PakBepeStore/env"
+set -gx VIRTUAL_ENV "/Users/user/Documents/shopping_list/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
