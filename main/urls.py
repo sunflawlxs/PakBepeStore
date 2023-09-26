dfb7f186f80cff59466c7fc3844016e17c351e1e
sunflawlxs <sherylivana99@gmail.com>
2023-09-26 13:09:17 +0700

    tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from main.views import show_main, create_product
 from django.urls import path
 from main.views import show_main
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import show_main, create_product, show_xml 
+from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -9,4 +12,7 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('xml/', show_xml, name='show_xml'),
     path('create-product', create_product, name='create_product'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,12 +2,22 @@ from main.models import Product
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
 
     # if products:
     #     last_products = products.last()
@@ -17,19 +27,56 @@ def show_main(request):
 
     context = {
         'AppName': 'PakBepeStore' ,
-        'name': 'Sheryl', # Nama kamu
+        'name': request.user.username,
         'class': 'PBP D', # Kelas PBP kamu
+        'NPM': '2206824943',
         'products': products,
+        'last_login': request.COOKIES['last_login'],
       #  'last_products': last_products,
     }
 
     return render(request, "main.html", context)
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
 def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
